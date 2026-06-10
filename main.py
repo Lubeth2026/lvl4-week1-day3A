abd0c30292a528ea24f50ba952449614be1a9137
Lubeth <lubethjimenez@yahoo.com>
2026-06-09 20:35:57 -0400

    Add created function calculate_total w/ a loop over numbers list adding total 1 number at a time

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 
 # List of Numbers through #'s variable
 numbers = [1, 15, 30, 45, 60]
-print(numbers)
+# print(numbers)
+
+# Function #1 named calculate_total
+def calculate_total(nums_list):
+    # Total variable to store the running total
+    total = 0
+
+    # This loop means go through every # in the list one at a time/ Add each # one by one starting from top
+    for number in nums_list:
+        total = total + number
+    return total
+
+
+total = calculate_total(numbers)
+print("The total is:", total)
