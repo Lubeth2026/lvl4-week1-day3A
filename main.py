a709c005edb4bcad69d0b7b8d5d51a88f14b1850
Lubeth <lubethjimenez@yahoo.com>
2026-06-09 21:04:56 -0400

    Add created function display_numbers w/ a loop over numbers list 1 at a time & print results to the terminal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+# print("Hello World")
+
 # List of Numbers through #'s variable
 numbers = [1, 15, 30, 45, 60]
 # print(numbers)
@@ -13,6 +15,16 @@ def calculate_total(nums_list):
         total = total + number
     return total
 
+# Function #2 named display_numbers
+def display_numbers(nums_list):
+    # Heading (print on terminal)
+    print("Numbers in the list:")
 
+    # This loop goes through every # in the list one at a time & prints the # on the terminal
+    for number in nums_list:
+        print(number)
+
+
+display_numbers(numbers)
 total = calculate_total(numbers)
 print("The total is:", total)
